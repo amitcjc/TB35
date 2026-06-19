2cdfc50e73e72c4f1ca7350efe9619afc5f7ea19
amitcjc <156275076+amitcjc@users.noreply.github.com>
2026-06-19 08:44:40 +0530

    by git user
    Add new Method and method name is m2

--- GitDemoTb35/src/com/Test.java
+++ GitDemoTb35/src/com/Test.java
@@ -8,6 +8,10 @@ public class Test
 		System.out.println("This is M1 Method");
 	}
 	
+	public void m2()
+	{
+		System.out.println("This is M2 Method");
+	}
 	
 	
 public static void main(String[] args) {
